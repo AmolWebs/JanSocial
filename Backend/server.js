a19de37ba85659c352facada2b40761629db67d5
Amol D <amoldangat836@gmail.com>
2025-09-07 15:40:23 +0530

    commint with confidential data

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,8 @@ import postRouter from './routes/post.route.js'
 import commentRouter from './routes/comment.route.js'
 import dotenv from 'dotenv'
 dotenv.config()
+
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 const corsOptions = {
@@ -18,11 +20,17 @@ app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
 app.use('/api/comment', commentRouter);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: 'Something went wrong' });
+});
+
 app.get('/', (req, res)=>{
     res.send('<h1>Hello Backend</h1>');
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("App is running on port 3000")
+    console.log(`App is running on port ${PORT}`)
 })
+
